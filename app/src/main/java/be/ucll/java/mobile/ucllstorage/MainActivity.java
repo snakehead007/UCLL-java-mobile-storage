885040b6586c589e9837d6003c1b9fd8283ca813
snakehead007 <snakehead007@pm.me>
2020-10-14 12:30:21 +0200

    Initial commit

---
+++ app/src/main/java/be/ucll/java/mobile/ucllstorage/MainActivity.java
@@ -0,0 +1,25 @@
+package be.ucll.java.mobile.ucllstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnSave;
+    private Button btnLoad;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+}
